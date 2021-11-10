fb9e3ee2f0cd4f077951f4a14edef4a58d519dc2
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2021-11-10 13:56:21 +0100

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -3,11 +3,14 @@ import { getRankings } from "../src/get-rankings.ts";
 
 export default async function (req: ServerRequest) {
   console.log(req.url);
-  const query = new URL(`http://example.com${req.url}`).searchParams;
-  const university = query.get("university");
+  const query = req.query
+  const university = query?.university;
   if (university === null) {
     return await req.respond({
       status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({
         error: "The university parameter is required",
       }),
